7bb1e72970a6053f3c27e05ce7dedadb195e7de4
John Manack <jmnck28@gmail.com>
2021-04-17 17:31:10 -0400

    adds debugging options

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,8 +5,17 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
+        // concatenation vs interpolation
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+                Console.WriteLine($"Hello, {args[1]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
