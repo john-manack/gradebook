0553b3b7236267560c1c730def6f706ad86d9366
John Manack <jmnck28@gmail.com>
2021-04-26 16:35:54 -0400

    adds testing

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         static void Main(string[] args)
-        // concatenation vs interpolation
         {
+            var book = new Book();
+            book.AddGrade(89.1);
+
+            // double[] numbers = new double[4] { 12.7, 10.3, 6.11, 4.1 }; <-- below is shorthand for this line
+            var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
+            // List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 }; <-- below is shorthand for this line
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
-                Console.WriteLine($"Hello, {args[1]}!");
-                Console.WriteLine($"Hello, {args[2]}!");
             }
             else
             {
